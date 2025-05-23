6ee4b3645b84888def5c025ce5cb3baff4502882
Juan Pablo Del Muro <jpdelmuro@gmail.com>
2025-05-22 18:07:47 -0600

    pequeña modificacion

--- Tareas/tareas.js
+++ Tareas/tareas.js
@@ -6,21 +6,26 @@ if (JSON.parse(localStorage.getItem("local")) == null){
 }else{
     arr = JSON.parse(localStorage.getItem("local"));
 }
-
 document.addEventListener("DOMContentLoaded",function(){
     let lista = document.getElementById("lista");
     for(let e of arr){
         let elemento = document.createElement("li");
         elemento.textContent = e;
+        elemento.addEventListener("click",function(){
+            lista.removeChild(elemento);
+            arr = arr.filter(function(item){
+                return item !==e;
+            });
+            localStorage.setItem("local",JSON.stringify(arr));
+        });
         lista.appendChild(elemento);
     }
 });
 
 
 /* Agregar elementos al LS */
-document.getElementById("agregar").addEventListener("click",function(e){
-    
-    e.preventDefault();
+document.getElementById("agregar").addEventListener("click",function(){
+
     let texto = document.getElementById("tarea").value;
     arr.push(texto);
     console.log(texto);
@@ -29,6 +34,5 @@ document.getElementById("agregar").addEventListener("click",function(e){
     elemento.textContent = texto;
     lista.appendChild(elemento);
     document.getElementById("tarea").value = '';
-
     localStorage.setItem("local",JSON.stringify(arr))
 });
