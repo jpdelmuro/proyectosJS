f711bddfe03e442aacce2d4e60b93cbafe428b06
Juan Pablo Del Muro <jpdelmuro@gmail.com>
2025-05-24 20:44:29 -0600

    tic tac toe

---
+++ Tic tac toe/gato.js
@@ -0,0 +1,13 @@
+let inicio = true;
+function opcion(elemento){
+    if(elemento.textContent !== 'X' && elemento.textContent !== 'O'){
+        if(inicio){
+            elemento.textContent = 'X';
+            inicio = false;
+        }else{
+            elemento.textContent = 'O';
+            inicio = true;
+        }
+    }
+
+}
